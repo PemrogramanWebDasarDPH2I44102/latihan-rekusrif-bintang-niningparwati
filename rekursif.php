750526eaaaede5ad2fe8a943c72d680a74141d37
niningparwati <42663455+niningparwati@users.noreply.github.com>
2018-09-13 09:01:03 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,47 @@
-<?php
+<?php 
+	$batasnya1= 10;
+	function rekursif ($a=1){
+			
+			Global $cbatasnya1;
+			echo "*";
+			$a++;
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
-}
-rekursif(5);
+    		if( $a<=$batasnya1){
+    			rekursif ($a);			
+			}
+			else{
+				echo "<br>";
+				$batasnya1--;
+
+				if($batasnya1!=0){
+					rekursif ();
+				}
+			}
+	}
+
+	rekursif();
+
+	/*========================================================*/
+ echo "<br>";
+
+	$batas =1;
+	function rekursif1($b=1){
+		Global $batas;
+		echo "$b";
+		$b++;
+
+		if ($b<=$batas) {
+			rekursif1($b);
+		}
+
+		else{
+			echo "<br>";
+			$batas++;
+			if ($batas <= 5) {
+				rekursif1();
+			}
+		}
+	}
+	rekursif1();
+ ?>5);
 ?>
